e23948cc5632f7bfd2585d37875c6a08d1975afb
Joanna Dyraga <joasia.dyraga@gmail.com>
2024-10-13 23:14:43 +0200

    Session countdown tests
    The first test checks the 10:00 countdown start
    after the user logs in. The second test checks
    whether the counter goes down.

---
+++ tests/session.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect, Page } from '@playwright/test';
+import { logIn } from '../test-utils/utils';
+
+test.describe('session time', () => {
+  test.beforeEach(async ({ page }) => {
+    await logIn(page)
+  });
+
+  async function getSessionLeftMinutes(page: Page) {
+    return await page.locator('#countdown_minutes').innerText();
+  };
+
+  async function getSessionLeftSeconds(page: Page) {
+    return await page.locator('#countdown_seconds').innerText();
+  };
+
+  async function getSessionLeft(page: Page) {
+    const minutes = await getSessionLeftMinutes(page)
+    const seconds = await getSessionLeftSeconds(page)
+    return `${minutes}:${seconds}`
+  };
+
+  test('10 minutes session after login', async ({ page }) => {
+    const timeout = await getSessionLeft(page)
+    await expect(timeout).toBe('10:00')
+  });
+
+  test('session time counts down', async ({ page }) => {
+    await page.waitForTimeout(1500)
+    const timeAfter = await getSessionLeft(page)
+    await expect(timeAfter).toBe('09:59')
+  })
+})
